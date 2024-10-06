5dd1e3e43409b4771724d66d2909d241cd8d74cd
L7R-a <gedylarosa@gmail.com>
2024-10-05 20:21:45 -0400

    Trying to make firebase to work

--- Frontend/src/Pages/Landing.js
+++ Frontend/src/Pages/Landing.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'primereact/resources/primereact.min.css';
 import { Toolbar } from 'primereact/toolbar';
 import { Button } from 'primereact/button';
@@ -6,43 +6,46 @@ import { Dialog } from 'primereact/dialog';
 import './Landing.css'; // Custom CSS file
 import Logo from '../assets/Logo.png';
 import MiniLogo from '../assets/MiniLogo.png';
-import { signInWithPopup } from 'firebase/auth';
+import { signInWithRedirect } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import ThemeSwitcher from '../SwitchTheme'; // Adjust the path accordingly
  
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId,
-    measurementId: process.env.REACT_APP_measurementId
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
 
 
 
 const Landing = () => {
+    const firebaseConfig = {
+        apiKey: process.env.REACT_APP_apiKey,
+        authDomain: process.env.REACT_APP_authDomain,
+        projectId: process.env.REACT_APP_projectId,
+        storageBucket: process.env.REACT_APP_storageBucket,
+        messagingSenderId: process.env.REACT_APP_messagingSenderId,
+        appId: process.env.REACT_APP_appId,
+        measurementId: process.env.REACT_APP_measurementId
+    };
+
+    // useEffect(() => {
+    //     console.log(firebaseConfig);
+    // }, []);
+    
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+    const provider = new GoogleAuthProvider();
+    
     const [visibleGoal, setVisibleGoal] = useState(false);
     const [visibleAbout, setVisibleAbout] = useState(false);
     const navigate = useNavigate(); // Initialize useNavigate hook
 
     const handleGoogleLogin = async () => {
-      try {
-        await signInWithPopup(auth, provider);
-        navigate('/home');
-
-      } catch (error) {
-        console.error("Error signing in with Google:", error);
-      }
-    };
-
+        try {
+           await signInWithRedirect(auth, provider); // Use signInWithRedirect instead of signInWithPopup
+           navigate('/home');
+        } catch (error) {
+           console.error("Error signing in with Google:", error);
+        }
+     };
     const startContent = (
         <div className="flex flex-wrap align-items-center gap-3">
             <img src={MiniLogo} alt="Logo" className="landing-mini-logo" />
@@ -52,9 +55,10 @@ const Landing = () => {
     const endContent = (
         <React.Fragment>
             <div className="flex align-items-center gap-2">
-                <Button label="Login" onClick={handleGoogleLogin} />
+                <Button label="Login" onClick={handleGoogleLogin} /> 
                 <ThemeSwitcher />
             </div>
+            {/* onClick={handleGoogleLogin} */}
         </React.Fragment>
     );
 
