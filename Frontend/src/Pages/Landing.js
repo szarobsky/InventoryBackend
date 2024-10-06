fee26adbf90b8c0e90d47682b7211070f5c009f3
L7R-a <gedylarosa@gmail.com>
2024-10-05 20:25:35 -0400

    Update Landing.js

--- Frontend/src/Pages/Landing.js
+++ Frontend/src/Pages/Landing.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import 'primereact/resources/primereact.min.css';
 import { Toolbar } from 'primereact/toolbar';
 import { Button } from 'primereact/button';
