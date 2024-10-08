b128c897cb0c561bb313004f6dddf1fa4222f389
szarobsky <szarob@me.com>
2024-10-07 23:16:32 -0400

    fixing font error

--- Frontend/src/firebaseConfig.js
+++ Frontend/src/firebaseConfig.js
@@ -1,15 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { GoogleAuthProvider, connectAuthEmulator, getAuth,  } from "firebase/auth";
 
-console.log("Environment Variables:");
-console.log("REACT_APP_apiKey:", process.env.REACT_APP_apiKey);
-console.log("REACT_APP_authDomain:", process.env.REACT_APP_authDomain);
-console.log("REACT_APP_projectId:", process.env.REACT_APP_projectId);
-console.log("REACT_APP_storageBucket:", process.env.REACT_APP_storageBucket);
-console.log("REACT_APP_messagingSenderId:", process.env.REACT_APP_messagingSenderId);
-console.log("REACT_APP_appId:", process.env.REACT_APP_appId);
-console.log("REACT_APP_measurementId:", process.env.REACT_APP_measurementId);
-
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_apiKey,
     authDomain: process.env.REACT_APP_authDomain,
@@ -21,7 +12,6 @@ const firebaseConfig = {
   };
   
   const app = initializeApp(firebaseConfig);
-  console.log(firebaseConfig);
   const provider = new GoogleAuthProvider();
   const auth = getAuth(app);
   if (window.location.hostname === "localhost") {
