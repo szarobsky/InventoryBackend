df1a6b87c898fd8b0788eaf2f48d4846788e891e
szarobsky <szarob@me.com>
2024-10-07 22:48:33 -0400

    firebase config fixing internal comma

--- Frontend/src/firebaseConfig.js
+++ Frontend/src/firebaseConfig.js
@@ -1,14 +1,23 @@
 import { initializeApp } from 'firebase/app';
 import { GoogleAuthProvider, connectAuthEmulator, getAuth,  } from "firebase/auth";
 
+
+const apiKey = process.env.REACT_APP_apiKey
+const authDomain = process.env.REACT_APP_authDomain
+const projectId = process.env.REACT_APP_projectId
+const storageBucket = process.env.REACT_APP_storageBucket
+const messagingSenderId = process.env.REACT_APP_messagingSenderId
+const appId = process.env.REACT_APP_appId
+const measurementId = process.env.REACT_APP_measurementId
+
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId,
-    measurementId: process.env.REACT_APP_measurementId
+    apiKey: apiKey,
+    authDomain: authDomain,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId,
+    measurementId: measurementId
   };
   
   const app = initializeApp(firebaseConfig);
