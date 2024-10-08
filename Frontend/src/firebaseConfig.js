9256c64cee60f3af672cbc5492a59fc477548e96
szarobsky <szarob@me.com>
2024-10-07 22:32:49 -0400

    adding in firebaseConfig

---
+++ Frontend/src/firebaseConfig.js
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import { GoogleAuthProvider, connectAuthEmulator, getAuth,  } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId
+  };
+  
+  const app = initializeApp(firebaseConfig);
+  console.log(firebaseConfig);
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth(app);
+  if (window.location.hostname === "localhost") {
+    connectAuthEmulator(auth, "http://localhost:9099");
+  }  
+  export { auth, provider }
