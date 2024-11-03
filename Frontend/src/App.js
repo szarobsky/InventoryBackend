785f56a28754261e7cabde3d60bc0e56740893a3
szarobsky <szarob@me.com>
2024-11-03 00:57:00 -0400

    updated router

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router >
       <Routes>
-        <Route path="/Home" element={<Home/>}/>
+        <Route path="/home" element={<Home/>}/>
         <Route path="/" element={<Landing />} />
         <Route path="*" element={<Landing/>} />
       </Routes>
