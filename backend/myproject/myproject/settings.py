9d71b4f7b196195182e651e84dd4515ae7d5e7e5
szarobsky <szarob@me.com>
2024-11-03 22:28:11 -0500

    testing csrf token

--- backend/myproject/myproject/settings.py
+++ backend/myproject/myproject/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-$0*q_%gdk@63$2@h*q=4+9t3$p)l9g3@=bh4^hq46jkvkud9!d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['inventorykh2024-backend-fta8gwhqhwgqfchv.eastus-01.azurewebsites.net', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', 'inventorykh2024-backend-fta8gwhqhwgqfchv.eastus-01.azurewebsites.net', '127.0.0.1']
 
 
 # Application definition
