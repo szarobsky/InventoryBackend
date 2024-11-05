8392f2ec1a7686e598b6442c2bf041b816c2b310
szarobsky <szarob@me.com>
2024-11-05 01:11:26 -0500

    changed scrollable height

--- Frontend/src/Pages/Home.js
+++ Frontend/src/Pages/Home.js
@@ -387,7 +387,7 @@ const Home = () => {
                         onChange={(e) => setSearchString(e.target.value)}
                         style={{ marginBottom: '10px' }}
                     />
-                    <DataTable value={items.filter(item => item.name.toLowerCase().includes(searchString.toLowerCase()))} emptyMessage="No items" scrollable scrollHeight="500px">
+                    <DataTable value={items.filter(item => item.name.toLowerCase().includes(searchString.toLowerCase()))} emptyMessage="No items" scrollable scrollHeight="400px">
                         <Column field="name" header="Item"></Column>
                         <Column field="date" header="Date" sortable></Column>
                         <Column body={actionBodyTemplate} header="Actions" />                 
