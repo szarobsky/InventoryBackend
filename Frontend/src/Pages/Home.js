7f780c5026e753c53a2039ed51fc35d51643f5f8
szarobsky <szarob@me.com>
2024-11-06 15:34:23 -0500

    removing cancel button

--- Frontend/src/Pages/Home.js
+++ Frontend/src/Pages/Home.js
@@ -392,7 +392,6 @@ const Home = () => {
     //Footer content for the Add Item dialog
     const footerContent = (
         <div>
-            <Button label="Cancel" icon="pi pi-times" onClick={() => setVisibleAddItem(false)} className="p-button-text" />
             <Button label="Save" icon="pi pi-check" onClick={addItem} />
         </div>
     );
@@ -446,7 +445,7 @@ const Home = () => {
                         <Button label="Scan Barcode" onClick={() => setVisibleBarcodeScanner(true)} />
                     </div>
                     <div className="p-field">
-                        <label htmlFor="itemDate">Date (leave default for N/A)</label>
+                        <label htmlFor="itemDate">Date (leave default 01/01/2024 11:59PM for N/A)</label>
                         <input id="itemDate" type="datetime-local" className="p-inputtext p-component" value={newItemDate} onChange={(e) => setNewItemDate(e.target.value)} />
                     </div>
                 </div>
